110f66b1ee25814300d26a4d1296318ceceec9d5
zaky <ahmed6mohamed6@gmail.com>
2022-10-20 09:27:23 +0200

    add deploy script for dev environment server

---
+++ Deploy/Script/deploy-dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo -e '\n\e[1m\e[34m Entering into backend directory...\e[0m\n'
+
+cd /var/www/server
+#php artisan down || true
+echo -e $PWD
+
+echo -e '\n\e[1m\e[34m Bitbucket Credentials...\e[0m\n'
+
+cat ~/password
+
+echo -e '\n\e[1m\e[34m Pulling code from remote...\e[0m\n'
+sudo git pull origin dev
+
+echo -e '\n\e[1m\e[34m Running Migrations...\e[0m\n'
+sudo  php artisan migrate
+
+
+#sudo composer dump-autoload
+ sudo php artisan route:clear; sudo  php artisan view:clear;sudo  php artisan cache:clear;
+
+#php artisan up
+
+echo -e '\n\e[1m\e[34mAll done now!\e[0m\n'
+echo -e '\n\e[1m\e[34m Thanks for automating this deployment process!\e[0m\n'
