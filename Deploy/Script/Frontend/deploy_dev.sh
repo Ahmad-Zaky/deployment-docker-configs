6ce0fae3779873b267f9324fe0c4a01d13a7dd3e
zaky <ahmed6mohamed6@gmail.com>
2022-10-25 16:05:47 +0200

    update frontend deploy bash script file

--- Deploy/Script/Frontend/deploy_dev.sh
+++ Deploy/Script/Frontend/deploy_dev.sh
@@ -3,55 +3,57 @@
 # Check if arguments are missing.
 if [ $# -eq 0 ]
   then
-  echo "No arguments supplied"
+  echo -e "\n\e[1;31m ERROR: No arguments supplied \033[0m"
   exit 0
 fi
 
 # Check if first argument is a number
 re='^[0-9]+$'
-if ! [[ $1 =~ $re ]] ; then
-  echo "\nerror: Not a number" >&2;
+if ! [[ $1 =~ $re ]]
+  then
+  echo -e "\n\e[1;31mERROR: Not a number \033[0m"
   exit 0
 fi
 
 # Check if zip command exists
 if ! command -v zip &> /dev/null
-then
-    echo "zip could not be found"
-    exit 0
+  then
+  echo -e "\n\e[1;31m ERROR: zip could not be found \033[0m"
+  exit 0
 fi
 
 appDir=$2
 versionsDir=$appDir/../versions
+branch=dev
 
-echo "\n #1 - Pull Frontend Repository START ..."
+echo -e "\n\e[1;32m #1 - Pull Frontend Repository START ... \033[0m"
 cd $appDir
-git checkout dev
-git pull origin dev
-echo "\n #1 - Pull Frontend Repository END ..."
+git checkout $branch
+git pull origin $branch
+echo -e "\n\e[1;32m #1 - Pull Frontend Repository END ... \033[0m"
 
 
 
-echo "\n #2 - Install Frontend Dependancies START ..."
+echo -e "\n\e[1;32m #2 - Install Frontend Dependancies START ... \033[0m"
 npm install
-echo "\n #2 - Install Frontend Dependancies END ..."
+echo -e "\n\e[1;32m #2 - Install Frontend Dependancies END ... \033[0m"
 
 
 
-echo "\n #3 - Build Frontend START ..."
+echo -e "\n\e[1;32m #3 - Build Frontend START ... \033[0m"
 
 # Check if dist folder exists
 [ -d dist ] && rm -rf dist
 npm run build
 
-echo "\n #3 - Build Frontend END ..."
+echo -e "\n\e[1;32m #3 - Build Frontend END ... \033[0m"
 
 
 
-echo "\n #4 - Versioning Frontend START ..."
+echo -e "\n\e[1;32m #4 - Versioning Frontend START ... \033[0m"
 # Ref. https://www.baeldung.com/linux/count-files-remove-oldest
 
-echo "\n Replace Current Version with the new version"
+echo -e "\n\e[1;32m Replace Current Version with the new version \033[0m"
 
 # Move up one level to application root
 cd $appDir/..
@@ -61,39 +63,39 @@ mkdir -p $versionsDir
 
 # Check if dist folder exists to move it to versions folder
 if [ -d dist ]
-then
-    zip -r dist.zip dist
-    mv dist.zip versions/dist_$(date +%d%m%Y-%H%M%S).zip
-    rm -rf dist
+  then
+  zip -r dist.zip dist
+  mv dist.zip versions/dist_$(date +%d%m%Y-%H%M%S).zip
+  rm -rf dist
 fi
 
 
 # Check if dist foldre exists in the application directory
 if [ ! -d $appDir/dist ]
-then
-    echo "dist folder does not exit in app folder"
-    exit 0
+  then
+  echo -e "\n\e[1;31m ERROR: dist folder does not exit in app folder \033[0m"
+  exit 0
 fi
 
 # move new build dist folder up to application root
 mv $appDir/dist .
 
-echo "\n Remove Old Versions"
+echo -e "\n\e[1;32m Remove Old Versions \033[0m"
 
 # The script receives the limit as an argument.
 limit=$1
 
 number_of_files=0
 if [ -n "$(ls -A $versionsDir/dist_* 2>/dev/null)" ]
-then
+  then
   number_of_files=$(ls $versionsDir/dist_* | wc -l)
 fi
 
 if [ $number_of_files -gt $limit ]
-then
-    # There are more files than the limit
-    # So we need to remove the older ones.
-    ls -1td dist_* | tail --lines=+$(expr $limit + 1) | xargs -d '\n' rm
+  then
+  # There are more files than the limit
+  # So we need to remove the older ones.
+  ls -1td dist_* | tail --lines=+$(expr $limit + 1) | xargs -d '\n' rm
 fi
 
-echo "\n #4 - Versioning Frontend Done ..."
+echo -e "\n\e[1;32m #4 - Versioning Frontend Done ... \033[0m"
